f581a540f64495cbfa213a0bec737d6a993bde37
wesly goris <wesly goris>
2017-02-15 21:26:40 -0600

    Module 9 work in progress

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import numberal from 'numeral';
+import'./index.css';
+
+const courseValue = numberal(1000).format('$0,0.00');
+debugger;
+
+console.log(`I would pay'  ${courseValue} for this awesome course!`);
+
+
